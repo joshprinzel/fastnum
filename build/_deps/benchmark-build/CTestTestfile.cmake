01dd567bd1035abcf20f5091f912db8d3a953b6a
joshprinzel <158182715+joshprinzel@users.noreply.github.com>
2025-12-30 20:46:51 -0500

    Repo Hygiene

--- build/_deps/benchmark-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/joshp_ya/VSCodeProjects/Personal/fast_stats/build/_deps/benchmark-src
-# Build directory: /mnt/c/Users/joshp_ya/VSCodeProjects/Personal/fast_stats/build/_deps/benchmark-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- build/_deps/benchmark-build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/joshp_ya/VSCodeProjects/Personal/fast_stats/build/_deps/benchmark-src/src
-# Build directory: /mnt/c/Users/joshp_ya/VSCodeProjects/Personal/fast_stats/build/_deps/benchmark-build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
